ad187e2da73d4db66861d2b1709c5ff37468b238
gretmn102 <erlgreywatson@gmail.com>
2023-05-21 02:48:34 +0300

    build: adding hash to assets.svg

--- src/App.fs
+++ src/App.fs
@@ -3,6 +3,9 @@ open Browser.Dom
 open Browser.Types
 open Browser.XMLDom
 
+let assetsUrl : string =
+    Fable.Core.JsInterop.importDefault "../assets/assets.svg"
+
 let canvas : HTMLDivElement = document.createElement("div") :?> HTMLDivElement
 
 let appNode = document.querySelector "#app"
@@ -14,8 +17,7 @@ canvas.innerText <- "Loading..."
 canvas.setAttribute("style", "height: 100%;")
 
 let loadSvg () =
-    let url = "/assets/assets.svg"
-    Fetch.fetch url []
+    Fetch.fetch assetsUrl []
     |> Promise.bind (fun x ->
         x.text()
     )
@@ -25,13 +27,11 @@ let loadSvg () =
         let svg = doc.documentElement :?> SVGElement
         svg.setAttribute("width", "100%")
         svg.setAttribute("height", "100%")
-        // svg.setAttribute("preserveAspectRatio", "xMaxYMax")
-
         svg
         |> Ok
     )
     |> Promise.catch (fun x ->
-        Error (sprintf "error load %s\n%s" url x.Message)
+        Error (sprintf "error load %s\n%s" assetsUrl x.Message)
     )
 
 loadSvg ()
